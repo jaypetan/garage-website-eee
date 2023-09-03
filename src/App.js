d206b9f0abe73c49354c9bc3333d857672f99efe
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2023-09-03 16:10:52 +0800

    Changed hash router to browser router

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/ambassador-detail" element={<AmbassadorDetail />} />
-      <Route path="/garage-website/events" element={<Events />} />
+      <Route path="/events" element={<Events />} />
       <Route path="/events/:id" element={<EventDetail />} />
       <Route path="/projects" element={<Project />} />
       <Route path="/projects/:id" element={<ProjectDetail />} />
