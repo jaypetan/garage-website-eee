80f49fd039c6e1f98db02ff65c9c23e158e1b911
kab0o0m <jroot97@hotmail.com>
2023-10-24 00:54:13 +0800

    homepage loading screen scrolling solved, aligned ambassador page with figma

--- src/routes/ambassador-detail/AmbassadorDetail.js
+++ src/routes/ambassador-detail/AmbassadorDetail.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import Header from "../../components/header/Header";
@@ -9,60 +9,75 @@ import "./AmbassadorDetail.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-function AmbassadorDetail(){
-    const params = useParams()
-    const id = params.id
-    const [{state, isLoading}, setState] = useState({state: {}, isLoading: true});
+function AmbassadorDetail() {
+  const params = useParams();
+  const id = params.id;
+  const [{ state, isLoading }, setState] = useState({
+    state: {},
+    isLoading: true,
+  });
 
-    const apiDomain = "https://ntueeegarage.pythonanywhere.com/api/";
+  const apiDomain = "https://ntueeegarage.pythonanywhere.com/api/";
 
-    useEffect(() => {
-        const url = apiDomain + "ambassadors/" + String(id);
+  useEffect(() => {
+    const url = apiDomain + "ambassadors/" + String(id);
 
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url);
-                const json = await response.json();
-                console.log(json);
-                setState(currentState => ({
-                    state: json,
-                    isLoading: false,
-                }));
-            } catch (error) {
-                console.log("error", error);
-            }
-        };
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setState((currentState) => ({
+          state: json,
+          isLoading: false,
+        }));
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
 
-        fetchData();
-        AOS.init({
-            duration: 1500,
-        });
-        window.addEventListener('load', AOS.refresh);
-    }, []);
+    fetchData();
+    AOS.init({
+      duration: 1500,
+    });
+    window.addEventListener("load", AOS.refresh);
+  }, []);
 
-        return(
-            <div className="detail-page">
-                <Header/>
-                    {isLoading === true ? <Loading/> :
-                    <main className="detail-main">
-                        <div className="main-wrapper" data-aos="fade-up">
-                        {/* put your code below */}
-                        <h1 className="detail-header">{state.name} </h1>
-                        <br/>
-                        <img src={state.displayImageUrl} className="display-image" alt={"image of '" + state.name + "'"}></img>
-                        <br/>
-                        <p className="article">{state.desc}</p>
-                        <br/>
-
-                        <br/>
-                        {/* put your code above */}
-                        </div>
-                    </main>                    
-                }
-                <br/>
-                {isLoading === false ? <Footer/> : <p></p>}
+  return (
+    <div className="ambassador-page">
+      <Header />
+      {isLoading === true ? (
+        <Loading />
+      ) : (
+        <main className="detail-main">
+          <div className="main-wrapper" data-aos="fade-up">
+            {/* put your code below */}
+            <div className="detail-header-box">
+              <p className="detail-header">{state.name} </p>
+              <button className="detail-header-button">
+                <div className="detail-header-button-chevron-left">&#x2C2;</div>
+                <div className="detail-header-button-back">Back</div>
+              </button>
             </div>
-        );
-};
+
+            <img
+              src={state.displayImageUrl}
+              className="display-image"
+              alt={"image of '" + state.name + "'"}
+            ></img>
+            <br />
+            <p className="article">{state.desc}</p>
+            <br />
+
+            <br />
+            {/* put your code above */}
+          </div>
+        </main>
+      )}
+      <br />
+      {isLoading === false ? <Footer /> : <p></p>}
+    </div>
+  );
+}
 
 export default AmbassadorDetail;
