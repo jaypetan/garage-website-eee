e5b4b123accb5168bcbb663466e1d375fc16958c
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2023-12-26 21:31:28 +0800

    Adjusted variable values

--- src/components/typography/TypographyConfig.js
+++ src/components/typography/TypographyConfig.js
@@ -1,24 +1,24 @@
 const typographyConfig = {
   desktop: {
     banner: { fontSize: "96px", lineHeight: "112px", fontWeight: 500 },
-    heading: { fontSize: "64px", lineHeight: "80px", fontWeight: 700 },
-    smallHeading: { fontSize: "32px", lineHeight: "48px", fontWeight: 600 },
+    heading: { fontSize: "64px", lineHeight: "80px", fontWeight: 500 },
+    smallHeading: { fontSize: "32px", lineHeight: "48px", fontWeight: 500 },
     body: { fontSize: "24px", lineHeight: "36px", fontWeight: 400 },
-    subtitle: { fontSize: "14px", lineHeight: "20px", fontWeight: 400 },
+    subtitle: { fontSize: "14px", lineHeight: "20px", fontWeight: 300 },
   },
   tablet: {
     banner: { fontSize: "64px", lineHeight: "80px", fontWeight: 500 },
-    heading: { fontSize: "48px", lineHeight: "64px", fontWeight: 700 },
-    smallHeading: { fontSize: "24px", lineHeight: "36px", fontWeight: 600 },
+    heading: { fontSize: "48px", lineHeight: "64px", fontWeight: 500 },
+    smallHeading: { fontSize: "24px", lineHeight: "36px", fontWeight: 500 },
     body: { fontSize: "20px", lineHeight: "24px", fontWeight: 400 },
-    subtitle: { fontSize: "14px", lineHeight: "20px", fontWeight: 400 },
+    subtitle: { fontSize: "14px", lineHeight: "20px", fontWeight: 300 },
   },
   mobile: {
     banner: { fontSize: "48px", lineHeight: "64px", fontWeight: 500 },
-    heading: { fontSize: "32px", lineHeight: "48px", fontWeight: 700 },
-    smallHeading: { fontSize: "20px", lineHeight: "24px", fontWeight: 600 },
+    heading: { fontSize: "32px", lineHeight: "48px", fontWeight: 500 },
+    smallHeading: { fontSize: "20px", lineHeight: "24px", fontWeight: 500 },
     body: { fontSize: "18px", lineHeight: "20px", fontWeight: 400 },
-    subtitle: { fontSize: "14px", lineHeight: "20px", fontWeight: 400 },
+    subtitle: { fontSize: "14px", lineHeight: "20px", fontWeight: 300 },
   },
 };
 
