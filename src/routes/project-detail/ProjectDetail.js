be66cfea5c8d81430253b778510e456b25609497
daena <daenaheng242@gmail.com>
2024-02-07 05:52:50 +0800

    updated alignment and switched to common components

--- src/routes/project-detail/ProjectDetail.js
+++ src/routes/project-detail/ProjectDetail.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import LinkPreview from "../../components/link-preview/LinkPreview";
 import Loading from "../../components/loading/Loading";
 import ImageGallery from "../../components/image-gallery/ImageGallery";
+import Typography from "../../components/typography/Typography";
 
 import "./ProjectDetail.css";
 import AOS from "aos";
@@ -45,44 +47,59 @@ function ProjectDetail(){
         return(
             <div className="detail-page">
                 <Header/>
-                    {isLoading === true ? <Loading/> :
+                    {isLoading === true ? (<Loading/> ):(
                     <main className="detail-main">
                         <div className="main-wrapper" data-aos="fade-up">
                         {/* put your code below */}
-                        <h1 className="detail-header">{state.name} </h1>
-                        <h3 className="subheader">{state.desc}</h3>
+                        <div className="detail-header-box">
+                            <Typography variant="heading">{state.name}</Typography>
+                            <Link to="/projects"><button className="upper-back-button"> Back  </button></Link>
+                        </div>
+                        <div className="subheader">
+                            <Typography variant="smallHeading">{state.desc}</Typography>
+                        </div>
+                        <img 
+                            src={state.displayImageUrl} 
+                            className="display-image"
+                            alt={"image of '" + state.name + "'"}>
+                        </img>
+                        
                         <br/>
-                        <img src={state.displayImageUrl} className="display-image" alt={"image of '" + state.name + "'"}></img>
                         <br/>
-                        <br/>
+
                         <LinkPreview link={String(state.link)}/>
                         <br/>
-                        <p className="article">{state.article}</p>
+                        <div className="article">
+                            <Typography variant="body">{state.article}</Typography>
+                        </div>
                         <br/>
-                        {state.contentVideoUrl ? 
-                        <video className="video" controls>
-                            <source src={state.contentVideoUrl} type="video/mp4"/>
-                            <source src={state.contentVideoUrl} type="video/ogg"/>
-                            Your browser does not support the video tag.
-                        </video>
-                        :
-                        <p></p>
-                        }
-                        {state.contentImages ?
-                            <ImageGallery images={state.contentImages}/>
-                        :
+                        <div className="gallery-box">
+                            <div className="gallery-header">
+                                <Typography variant="smallHeading">Gallery</Typography>
+                            </div>
+                            {state.contentVideoUrl ? 
+                            <video className="video" controls>
+                                <source src={state.contentVideoUrl} type="video/mp4"/>
+                                <source src={state.contentVideoUrl} type="video/ogg"/>
+                                Your browser does not support the video tag.
+                            </video>
+                            :
                             <p></p>
-                        }
-                        
-                        
+                            }
+                            {state.contentImages ?
+                                <ImageGallery images={state.contentImages}/>
+                            :
+                             <p></p>
+                            }
+                        </div>
+                        </div>
 
                         <br/>
                         <br/>
                         <br/>
                         {/* put your code above */}
-                        </div>
                     </main>                    
-                }
+                )}
                 <br/>
                 {isLoading === false ? <Footer/> : <p></p>}
             </div>
