9ca7f0a4d350b1186e39f04fa402101fcf01ed2a
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-03-31 14:11:09 +0800

    Events and Projects Overview

---
+++ src/components/overviewPage/OverviewPage.js
@@ -0,0 +1,36 @@
+import BackButton from "../../components/BackButton/BackButton";
+import Grid from "../../components/grid/Grid";
+import Card from "../../components/PhotoCard/PhotoCard";
+import Typography from "../../components/typography/Typography";
+
+import styles from "./OverviewPage.module.css";
+
+const OverviewPage = ({ heading, data }) => {
+  return (
+    <div className={styles["content-wrapper"]}>
+      <div className={styles["heading-space"]}>
+        <Typography variant="heading">{heading}</Typography>
+        <BackButton />
+      </div>
+      {data && (
+        <>
+          <Grid>
+            {data.map((card, index) => (
+              <Card
+                key={card.name}
+                image={card.coverPic}
+                to={`${index}/`}
+                bottomText={card.name}
+              />
+            ))}
+          </Grid>
+          <button onClick={() => window.scrollTo(0, 0)}>
+            <Typography variant="body">Back to top</Typography>
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default OverviewPage;
