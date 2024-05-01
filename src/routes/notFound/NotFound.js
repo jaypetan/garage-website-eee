24f8f9db75a621c1be2f6eb3b72c674250f415b9
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-05-01 15:28:58 +0800

    Better 404 page

--- src/routes/notFound/NotFound.js
+++ src/routes/notFound/NotFound.js
@@ -1,11 +1,25 @@
-import React from "react"
-
-import './NotFound.css'
+import styles from "./NotFound.module.css";
+import PageTemplate from "../../components/pageTemplate/PageTemplate";
+import Typography from "../../components/typography/Typography";
+import Transition from "../../components/transition/Transition";
+import Button from "../../components/button/Button";
 
 function NotFound() {
-    return(
-        <h1>Page Not Found</h1>
-    )
+  return (
+    <Transition isLoading={false}>
+      <PageTemplate className={styles["template"]}>
+        <div className={styles["center"]}>
+          <Typography variant="banner">404</Typography>
+          <Typography variant="heading" textAlign="center">
+            Page not found
+          </Typography>
+          <Button to="/" className={styles["margin"]}>
+            Go Home
+          </Button>
+        </div>
+      </PageTemplate>
+    </Transition>
+  );
 }
 
 export default NotFound;
