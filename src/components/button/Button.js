95f8141adb639ca21500aeb15d0f18d37900cf0d
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-05-10 17:47:16 +0800

    forwardref on button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -2,45 +2,52 @@ import { Link } from "react-router-dom";
 import Typography from "../typography/Typography";
 
 import styles from "./Button.module.css";
+import { forwardRef } from "react";
 
-const Button = ({
-  children,
-  onClick,
-  startIcon,
-  endIcon,
-  to,
-  variant = "filled",
-  disabled = false,
-  className,
-  ...rest
-}) => {
-  // Use the btnStyle prop to determine the button class to be applied
-  const cn = [
-    className,
-    styles.btn, // Base button class
-    styles[variant],
-    disabled && styles.disabled,
-  ]
-    .filter(Boolean)
-    .join(" ");
+const Button = forwardRef(
+  (
+    {
+      children,
+      onClick,
+      startIcon,
+      endIcon,
+      to,
+      variant = "filled",
+      disabled = false,
+      className,
+      ...rest
+    },
+    ref
+  ) => {
+    // Use the btnStyle prop to determine the button class to be applied
+    const cn = [
+      className,
+      styles.btn, // Base button class
+      styles[variant],
+      disabled && styles.disabled,
+    ]
+      .filter(Boolean)
+      .join(" ");
 
-  const Comp = to ? Link : "button";
+    const Comp = to ? Link : "button";
 
-  return (
-    <Comp
-      className={cn}
-      to={to}
-      onClick={onClick}
-      disabled={disabled}
-      {...rest}
-    >
-      {startIcon && (
-        <div className={styles["start-icon-wrapper"]}>{startIcon}</div>
-      )}
-      <Typography variant="body">{children}</Typography>
-      {endIcon && <div className={styles["end-icon-wrapper"]}>{endIcon}</div>}
-    </Comp>
-  );
-};
+    return (
+      <Comp
+        className={cn}
+        to={to}
+        onClick={onClick}
+        disabled={disabled}
+        ref={ref}
+        {...rest}
+      >
+        {startIcon && (
+          <div className={styles["start-icon-wrapper"]}>{startIcon}</div>
+        )}
+        <Typography variant="body">{children}</Typography>
+        {endIcon && <div className={styles["end-icon-wrapper"]}>{endIcon}</div>}
+      </Comp>
+    );
+  }
+);
 
 export default Button;
