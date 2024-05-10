2b30abc0f7937d0922f7aeab87d450c60d96dd99
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-05-10 17:17:44 +0800

    Updated link preview styles

--- src/components/LinkPreview/LinkPreview.js
+++ src/components/LinkPreview/LinkPreview.js
@@ -1,32 +1,22 @@
 import Typography from "../typography/Typography";
-import "./LinkPreview.css";
+import styles from "./LinkPreview.module.css";
 
-function LinkPreview(props) {
-  let link = props.link;
-  let endProtocol = link.indexOf("//");
-  if (endProtocol !== -1) {
-    link = link.substring(endProtocol + 2);
-  }
-  let slashIndex = link.indexOf("/");
-  if (slashIndex !== -1) {
-    link = link.substring(0, slashIndex);
-  }
-
+function LinkPreview({ link }) {
   return (
     <a
-      href={props.link}
+      href={link}
       target="_blank"
       rel="noreferrer"
-      className="link-preview"
+      className={styles["link-preview"]}
     >
       <img
-        className="favicon"
-        src={"https://icons.duckduckgo.com/ip3/" + link + ".ico"}
+        className={styles["favicon"]}
+        src={`https://www.google.com/s2/favicons?domain=${link}&sz=128`}
         alt="url's favicon"
       />
-      <div className="link-label">
+      <div className={styles["link-label"]}>
         <Typography variant="smallHeading">Visit Us</Typography>
-        <Typography>{props.link}</Typography>
+        <Typography className={styles["truncate"]}>{link}</Typography>
       </div>
     </a>
   );
